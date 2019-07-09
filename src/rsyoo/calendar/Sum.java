175342f005d7b6eb91cdfcfee26be50765286274
ddf <ddf@200.200.100.111>
2019-07-09 20:46:27 +0900

    update sum

---
+++ src/rsyoo/calendar/Sum.java
@@ -0,0 +1,24 @@
+package rsyoo.calendar;
+
+import java.util.Scanner;
+
+public class Sum {
+	public static void main(String[] args) {
+		//입력 : 키보드로 두 수의 입력을 받는다
+		//출력 : 화면에 두수의 합을 출력한다. 
+		int a, b;
+		
+		Scanner scanner = new Scanner(System.in);
+		String s1, s2;
+		s1 = scanner.next();
+		s2 = scanner.next();
+		a = Integer.parseInt(s1);
+		b = Integer.parseInt(s2);
+		System.out.println(a +  "," + b);
+		
+		System.out.println("두수의 합은 " + ( a + b) + "입니다.");
+		System.out.printf("%d와 %d합은 %d입니다.", a, b, a + b);
+		scanner.close();
+		
+	}
+}
